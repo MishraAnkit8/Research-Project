34d443bd5d0cd757f940265c2224a7ddc5168789
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2023-11-23 12:32:03 +0530

    commit

--- Research-Project/Research-Html/js/script.js
+++ Research-Project/Research-Html/js/script.js
@@ -3,24 +3,50 @@ const closeToggleBtn = document.getElementById("closeToggleBtn");
 const openSideBar = document.getElementById("openSideBar");
 const mainContent = document.getElementById("mainContent");
 const navbarHeading = document.getElementById("navbarHeading");
+var hamburger = document.getElementById("hamburger");
+
+// Function to close the sidebar
+function closeSidebar() {
+    openSideBar.classList.remove("left-sidebar-open");
+    // Add other necessary class removals based on your code
+}
+
 closeToggleBtn.addEventListener("click", () => {
     openSideBar.classList.toggle("left-400");
     mainContent.classList.toggle("left-0");
-    navbarHeading.classList.toggle("left-0")
+    navbarHeading.classList.toggle("left-0");
     closeToggleBtn.classList.toggle("left-sidebar-toggle-turn");
 });
-// Show userProfile drop down
+
+// Show userProfile drop-down
 const defaultDropdown = document.getElementById("defaultDropdown");
-defaultDropdown.addEventListener("click", () => {
+defaultDropdown.addEventListener("click", (event) => {
     const showDropDown = document.getElementById("showDropDown");
     showDropDown.classList.toggle("show");
+    event.stopPropagation();
 
 });
 
-// show side-bar when click on hamburger
-let hamburger = document.getElementById("hamburger");
-hamburger.addEventListener("click", () => {
-    const openSideBar = document.getElementById("openSideBar");
-    openSideBar.classList.toggle("left-sidebar-open")
-    // alert("click")
+document.addEventListener('click', (event) => {
+    if(!showDropDown.contains(event.target) && !defaultDropdown.contains(event.target) && !hamburger.contains(event.target)){
+        showDropDown.classList.remove('show')
+    }
+
 })
+
+// Show side-bar when click on hamburger
+hamburger.addEventListener("click", (event) => {
+    openSideBar.classList.toggle("left-sidebar-open");
+    event.stopPropagation();
+});
+
+// Close sidebar when clicking anywhere outside of it
+document.addEventListener("click", (event) => {
+    console.log("Body click");
+    if (!openSideBar.contains(event.target) && !hamburger.contains(event.target)) {
+            openSideBar.classList.remove("left-sidebar-open");
+            console.log('closing bside bar ')
+        
+    }
+});
+
